3b9484fb430455c407e450984f7581a2aa372456
psrochat <pierre.samuel.rochat@gmail.com>
2017-03-02 13:48:56 +0100

    Fichiers matrice

---
+++ BinaryMatrix.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryMatrix.cpp
+//  lab13_matrice
+//
+//  Created by Pierre-Samuel Rochat on 02.03.17.
+//  Copyright © 2017 Pierre-Samuel Rochat. All rights reserved.
+//
+
+#include "BinaryMatrix.hpp"
---
+++ BinaryMatrix.hpp
@@ -0,0 +1,19 @@
+//
+//  BinaryMatrix.hpp
+//  lab13_matrice
+//
+//  Created by Pierre-Samuel Rochat on 02.03.17.
+//  Copyright © 2017 Pierre-Samuel Rochat. All rights reserved.
+//
+
+#ifndef BinaryMatrix_hpp
+#define BinaryMatrix_hpp
+
+#include <stdio.h>
+
+class BinaryMatrix {
+private:
+    
+};
+
+#endif /* BinaryMatrix_hpp */
